e81cb2981ea1a6a3069ae972d7e8162285f274b0
abundis-rmn2 <abundiscomunicacion@gmail.com>
2022-09-29 20:29:49 -0500

    ejemplos

--- tokenizer-loc-hash.py
+++ tokenizer-loc-hash.py
@@ -36,6 +36,22 @@ def mention_hashtags_set_extension(doc):
     Token.set_extension('is_mention', getter=mention_getter)
     return doc
 
+@Language.component("local_set_extension")
+def loc_set_extension(doc):
+    wordlist = initialize_words("wl")
+    Token.set_extension("is_geo", default=False)
+    Token.set_extension("geo_countrycode", default=None)
+    print(type(wordlist))
+    for token_index, token in enumerate(doc):
+        if token._.is_hashtag:
+            #print("token es hashtag en componente",token.text)
+            parse_tag(token, token.text, wordlist)
+            #if token.text in wordlist:
+            #    print("lo encontró")
+            #    print(token)
+    return doc
+
+#none
 # Returns a list of common english terms (words)
 def initialize_words(dict_txt):
     content = None
@@ -69,49 +85,42 @@ def parse_tag(token, term, wordlist):
     for tag in tags:
         print("Tags despues de split for each", tag)
         word = find_word(token, tag, wordlist)
-        while word != None and len(tag) < 3:
+        print("despues find_word")
+        while word != None and len(tag):
             words.append(word)
+            print("algo pasa?")
             if len(tag) == len(word): # Special case for when eating rest of word
                 break
             tag = tag[len(word):]
             word = find_word(token, tag, wordlist)
-    return " ".join(words)
+    return(" ".join(words))
 
 def find_word(token, tag, wordlist):
-    print("find_word:", tag)
+    print("find_word():", tag)
     i = len(tag) + 1
     while i > 1:
         i -= 1
         if tag[:i] in wordlist and len(tag[:i]):
-            print("buscando:",tag[:i])
+            print("find_word() inlist:",tag[:i])
             city = gc.search_cities(tag[:i], case_sensitive=False)
-            i = 0
-            while i < len(city):
-                if (city[i]['countrycode'] == "CA") or (city[i]['countrycode'] == "MX") or (city[i]['countrycode'] == "US"):
+            e = 0
+            while e < len(city):
+                if (city[e]['countrycode'] == "CA") or (city[e]['countrycode'] == "MX") or (city[e]['countrycode'] == "US"):
                     token._.is_geo = True
-                i += 1
+                    token._.set("geo_countrycode",city[e]['countrycode'])
+                    print(tag[:i], "is geo")
+                    print(city[e])
+                e += 1
+            return tag[:i]
     return None
 
 
-@Language.component("local_set_extension")
-def loc_set_extension(doc):
-    wordlist = initialize_words("wl")
-    Token.set_extension("is_geo", default=False)
-    print(type(wordlist))
-    for token_index, token in enumerate(doc):
-        if token._.is_hashtag:
-            #print("token es hashtag en componente",token.text)
-            parse_tag(token, token.text, wordlist)
-            #if token.text in wordlist:
-            #    print("lo encontró")
-            #    print(token)
-    return doc
 
 nlp.add_pipe("mention_hashtags", first=True)
 nlp.add_pipe("mention_hashtags_set_extension", after="mention_hashtags")
 nlp.add_pipe("local_set_extension", after="mention_hashtags_set_extension")
 
-s = """#winnipegbench #canadabench #jaliscograffiti #benchguadalajara #graffitiGuadalajara"""
+s = """#winnipegbench #canadabench #jaliscograffiti #benchguadalajara #bombasguadalajaramistrik #jasdjaws #jawscaminojalisco #tlajomulco #guadalajaragraffiti"""
 s = re.sub(r'#', r' #', s)
 doc = nlp(s)
 
@@ -121,4 +130,4 @@ for token in doc:
         if token._.is_geo:
             #time.sleep(5)
             #print(token.text, " - ", cl.user_info_by_username(token_mention).biography)
-            print(token.text, " - geo ")
+            print(token.text, " - geo -", token._.geo_countrycode)
